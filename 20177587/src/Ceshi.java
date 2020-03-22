fd157deeb74d0dda51560d91077789e26ed36bbc
YinChuanLiu <45043075+YinChuanLiu@users.noreply.github.com>
2020-03-22 23:13:11 +0800

    第三次作业

---
+++ 20177587/src/Ceshi.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class Ceshi {
+    @Test
+    public void test1(){
+        Sudoku.clear0(3,3,3);
+    }
+}
---
+++ 20177587/src/lib.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class lib {
+
+    //判断 数组在i行j列 是否能确定一个值
+	//写在Sudoku里了
+
+    public static boolean isUnique(int i,int j,int arr[]){
+
+    }
+}
